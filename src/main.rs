32ed4f66fedc41b08e78e1d7f6f03701e567e873
toxicmender <13049649+toxicmender@users.noreply.github.com>
2021-09-25 23:56:14 +0530

    feat(cmp): compares user input (converted to u32) with RNG number (also u32)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the Number!");
 
-    let secret = rand::thread_rng().gen_range(1..101);
+    let secret: u32 = rand::thread_rng().gen_range(1..101);
 
     println!("The Secret number: {}", secret);
 
@@ -14,5 +15,13 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("too small!"),
+        Ordering::Greater => println!("too big!"),
+        Ordering::Equal => println!("You Win!"),
+    }
     println!("You guessed: {}", guess);
 }
